04db3bd84be53bf4371b4911b322224bcf4a0209
vinMoreiraHenrique <viniciusmoreirahenrique@gmail.com>
2023-04-14 20:55:30 -0300

    feat: added the hover bottom animation on link list

--- test_wiccom/src/components/header/styles.ts
+++ test_wiccom/src/components/header/styles.ts
@@ -2,8 +2,6 @@ import styled from "styled-components";
 import { theme } from "../../styles/theme";
 
 export const StyledHeader = styled.header`
-  
-  
   width: 1280px;
   background-color: ${theme.colors.black1};
   padding: 20px 56px;
@@ -29,12 +27,27 @@ export const StyledNavBar = styled.nav`
 
     li {
       display: flex;
+      flex-direction: column;
       justify-content: center;
       width: 103px;
       height: 72px;
+      position: relative;
       transition: 0.5s;
       cursor: pointer;
       align-items: center;
+      &::after {
+        content: "";
+        position: absolute;
+        bottom: 0;
+        left: 0;
+        width: 0%;
+        height: 3px;
+        background-color: ${theme.colors.orange1};
+        transition: width 0.3s ease-in-out;
+      }
+      &:hover::after {
+        width: 100%;
+      }
       &:hover {
         background-color: ${theme.colors.gray1};
         &:hover {
