3dedb72b3b791933d77cac0fd629d81171e55c4d
vinMoreiraHenrique <viniciusmoreirahenrique@gmail.com>
2023-04-16 19:00:23 -0300

    feat: productCarousel features finished, added star icon and cart icon in the squareProductContainer

--- test_wiccom/src/components/ProductCardSquare/index.tsx
+++ test_wiccom/src/components/ProductCardSquare/index.tsx
@@ -7,6 +7,9 @@ import {
 } from "./styles";
 import SelectableButton from "../SelectableButton";
 import { ProductsCarouselHiddenMenu } from "../ProductsCarouselHiddenMenu";
+import { AiOutlineStar, AiTwotoneStar } from "react-icons/ai";
+import { BsCart3 } from "react-icons/bs";
+import { theme } from "../../styles/theme";
 
 interface IProductCard {
   categoryText: string;
@@ -17,11 +20,49 @@ export const ProductCardSquare = ({
   categoryText,
   modelName,
 }: IProductCard) => {
+  const [isYellow, setIsYellow] = useState(false);
+
+  function handleClick() {
+    setIsYellow(!isYellow);
+  }
   return (
     <StyledOuterDiv>
       <StyledProductsSquareContainer>
         <ProductsCarouselHiddenMenu modelName={modelName} />
         <StyledProductsSquareCarouselContainer>
+          <div
+            className="iconDiv"
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              position: "absolute",
+              color: theme.colors.gray1,
+              right: "3%",
+              top: "5%",
+              gap: "10px",
+            }}
+          >
+            {isYellow ? (
+              <AiTwotoneStar
+                style={{ width: "24px", height: "24px", cursor: "pointer" }}
+                className="favorite"
+                color={theme.colors.orange1}
+                onClick={handleClick}
+              />
+            ) : (
+              <AiOutlineStar
+                style={{ width: "24px", height: "24px", cursor: "pointer" }}
+                className="favorite"
+                color={theme.colors.gray1}
+                onClick={handleClick}
+              />
+            )}
+
+            <BsCart3
+              style={{ width: "24px", height: "24px", cursor: "pointer" }}
+            />
+          </div>
           <img src="src\assets\mainImgs\product2.png" alt="Whey protein" />
         </StyledProductsSquareCarouselContainer>
         <div>
